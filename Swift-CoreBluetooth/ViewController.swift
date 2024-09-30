1a1be1971b5fafef046148ebb6479429fa09a36b
Suyeon <syhhwang1231@gmail.com>
2024-09-30 21:07:59 +0900

    Initial commit

---
+++ Swift-CoreBluetooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-CoreBluetooth
+//
+//  Created by Suyeon Hwang on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
